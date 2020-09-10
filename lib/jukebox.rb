dcfb544f8845605ef876aea15cd5e1ef542b4497
Jason Easterly <jason.easterly11@gmail.com>
2020-09-10 20:10:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,19 +1,39 @@
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 
 
 def welcome
   puts "Please enter a command:"
 end
 
-def help()
-
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays the help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits the program"
 end 
 
 def play
   
 end
 
-def list
-
+def list(songs)
+  list = []
+  songs.each_with_index do |song_list|
+    list << song_list + 1
+  end
+list
 end
 
 def exit_jukebox
@@ -23,4 +43,5 @@ end
 
 def run(command)
   welcome
-end  
+  help
+end
