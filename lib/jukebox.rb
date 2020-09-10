31d85925d08140be3d9c0593d2d49d103253480f
Jason Easterly <jason.easterly11@gmail.com>
2020-09-10 22:57:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,7 +24,7 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_response = gets.chomp 
+  user_input = gets.strip 
   
     if (1..9).to_a.include?(user_response.to_i)
       puts "Playing #{songs[user_response.to_i - 1]}"
