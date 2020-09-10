bfc15d4819bda5ea34f6e613fe38125e39b27b14
Jason Easterly <jason.easterly11@gmail.com>
2020-09-10 20:28:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,7 +19,7 @@ end
 
 def help
   puts "I accept the following commands:"
-  puts "- help : displays the help message"
+  puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
   puts "- exit : exits the program"
