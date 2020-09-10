f6810a9ac72ac5319fe9f936fdf836d21d0693e4
Jason Easterly <jason.easterly11@gmail.com>
2020-09-10 20:36:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,8 +32,8 @@ def play(songs)
   songs.each_with_index do |song_name, song_number|
     song_number += song_number + 1
     if user_input == song_name || user_input == song_number
-#binding.pry
       puts "Playing #{song_name}"
+    binding.pry
     else
       puts "Invalid input, please try again"
     end
