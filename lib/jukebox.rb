a3a41d2975ec7d8cf3d7f8a8866907f16546486b
Jason Easterly <jason.easterly11@gmail.com>
2020-09-10 21:29:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,14 +28,13 @@ def play(songs)
   answer = ""
   songs.each_with_index do |song_name, song_number|
     if user_input == song_name 
-      answer = "Playing #{song_name}"
+      return "Playing #{song_name}"
     elsif user_input == (song_number + 1).to_s
-      answer = "Playing #{song_name}"
+      return "Playing #{song_name}"
     else
-      answer = "Invalid input, please try again"
+      return "Invalid input, please try again"
     end
   end
-puts answer  
 binding.pry
 end
 
